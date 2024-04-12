e597a5edf628767e292cd328e5083f3974a4e4d9
yamoo9 <yamoo9@naver.com>
2024-04-12 14:04:09 +0900

    [구성] 개발 환경 (with Vite)

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const { BASE_URL, DEV_SERVER_PORT } = env;
+
+  return {
+    base: BASE_URL ?? '/',
+    server: {
+      host: 'localhost',
+      port: DEV_SERVER_PORT,
+      cors: true,
+      open: true,
+    },
+  };
+});
