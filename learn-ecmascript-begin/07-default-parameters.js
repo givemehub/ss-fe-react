6d752095f0fbb1b194211739e127fe611d4f1506
yamoo9 <yamoo9@naver.com>
2024-04-15 13:25:27 +0900

    [학습] 기본, 나머지 매개변수

--- learn-ecmascript-begin/07-default-parameters.js
+++ learn-ecmascript-begin/07-default-parameters.js
@@ -3,18 +3,31 @@
 
 // 🔶 기본값 매개변수를 활용하고, nullish 연산자를 제거하세요.
 // 참고: https://mzl.la/3JkRZBH
-const randomNumber = (min, max) => {
-  min = min ?? 0;
-  max = max ?? 10;
+// const randomNumber = (min = 0, max = 10) => {
+// const randomNumber = (options) => {
+// const randomNumber = (options = {}) => {
+// const randomNumber = ({ min, max } = {}) => {
+// const randomNumber = ({ min = 0, max = 10 } = {}) => {
+const randomNumber = ({ min: initMin = 0, max = 10 } = {}) => {
+  // console.log(min, max);
 
-  return Math.round(Math.random() * (max - min)) + min;
+  // console.log(options);
+  // min = min ?? 0;
+  // max = max ?? 10;
+
+  // const config = { min: 0, max: 10, ...options };
+  // console.log(config);
+
+  // const { min, max } = config;
+
+  return Math.round(Math.random() * (max - initMin)) + initMin;
 };
 
-let n1 = randomNumber();
+let n1 = randomNumber(); /* options = undefined */
 console.log(n1, n1 >= 0 && n1 <= 10);
 
-let n2 = randomNumber(5);
+let n2 = randomNumber({ min: 5 } /* options {} */);
 console.log(n2, n2 >= 5 && n2 <= 10);
 
-let n3 = randomNumber(5, 7);
+let n3 = randomNumber({ max: 7, min: 5 });
 console.log(n3, n3 >= 5 && n3 <= 7);
--- learn-ecmascript-begin/08-rest-parameters.js
+++ learn-ecmascript-begin/08-rest-parameters.js
@@ -1,6 +1,20 @@
 // --------------------------------------------------------------------------
 // rest parameters
 
+// 전개 구문
+const list = ['react', 'redux'];
+const combineList = ['zustand', ...list, 'jotai'];
+
+console.log(combineList);
+
+// 나머지 매개변수 (함수의 매개변수 영역) -> 배열로 반환
+function callListItem(fisrtItem, ...items /* items = [one, two, three] */) {
+  console.log(items);
+}
+
+// 전개 구문
+callListItem(...combineList);
+
 function _sum() {
   const numbers = Array.from(arguments);
   return numbers.reduce((result, number) => result + number, 0);
@@ -8,7 +22,7 @@ function _sum() {
 
 // 🔶 나머지 매개변수를 사용해 sum 함수 코드 로직을 다시 작성합니다.
 // 참고: https://mzl.la/43Ro9yp
-const sum = () => {};
+const sum = (...numbers) => numbers.reduce((total, count) => total + count);
 
 let result1_1 = _sum(2, 3, 9, 12, 105);
 let result1_2 = sum(2, 3, 9, 12, 105);
