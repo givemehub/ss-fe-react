58a2193679d18d805a3107dc81b71ec5cdd6f181
yamoo9 <yamoo9@naver.com>
2024-04-16 15:44:23 +0900

    [학습] JSX 구문 활용
    - jsconfig.json
    - JSX 데이터, 속성 바인딩
    - JSX 속성 전개 구문

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Component } from 'react';
+import { jsx } from 'react/jsx-runtime';
 import { Description, Headline, Link } from './components';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state /* component data */ = {
+      container: {
+        className: 'learn',
+      },
       headline: '리액트 러닝 가이드',
       description:
         '리액트 라이브러리를 사용해 사용자 인터페이스를 구축하는 방법을 학습합니다.',
@@ -19,13 +23,30 @@ class App extends Component {
   }
 
   render() {
-    const { headline, description, link } = this.state;
+    const {
+      container: { className: mainClassName },
+      headline,
+      description,
+      link,
+    } = this.state;
     const { text: linkText, ...restLinkProps } = link;
+    // const { className, href, target, rel } = restLinkProps;
 
     return (
-      <main className="learn">
+      <main className={mainClassName}>
+        {/* 슬롯(slot) = props.children */}
         <Headline>{headline}</Headline>
+        {/* 기본 슬롯(default slot) = props.children */}
         <Description>{description}</Description>
+        {/* classic: React.createElement */}
+        {/* automatic: React.jsx */}
+        {/* {jsx(Link, {
+          ...restLinkProps,
+          children: linkText,
+        })} */}
+        {/* <Link className={className} href={href} rel={rel} target={target}>
+          {linkText}
+        </Link> */}
         <Link {...restLinkProps}>{linkText}</Link>
       </main>
     );
--- src/components/link.jsx
+++ src/components/link.jsx
@@ -1,7 +1,15 @@
-export function Link(props) {
+export function Link({ className, href, rel, target, children }) {
   return (
-    <a href={props.href} target={props.target} rel={props.rel}>
-      {props.children}
+    <a
+      // 속성 바인딩
+      className={className}
+      href={href}
+      target={target}
+      rel={rel}
+    >
+      {/* 데이터 바인딩(연결) */}
+      {/* 기본 슬롯 (default slot) */}
+      {children}
     </a>
   );
 }
