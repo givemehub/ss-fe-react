37d3751e08b60783dec115b8a63be30f07d06f2b
yamoo9 <yamoo9@naver.com>
2024-04-17 10:12:38 +0900

    [실습] Link 컴포넌트 테스트

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -1,6 +1,7 @@
 export function Link({ href, target, rel, className, children, ...restProps }) {
   return (
     <a
+      data-testid="link"
       href={href}
       target={target}
       rel={rel}
--- tests/components/link.test.jsx
+++ tests/components/link.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { Link } from '../../src/components';
 
 describe('Link 컴포넌트', () => {
-  it('<a> 링크 요소 렌더링', () => {
+  it('<a href> 링크 요소 렌더링', () => {
     render(<Link href="/">홈</Link>);
 
     const link = screen.getByRole('link', { name: /홈/ });
@@ -36,5 +36,21 @@ describe('Link 컴포넌트', () => {
     const link = screen.getByRole('link', { name: /리액트/ });
     expect(link).toHaveAttribute('rel');
     expect(link).toHaveAttribute('target', '_blank');
+  });
+
+  it('주어진 속성 외 추가 속성 확장 렌더링', () => {
+    render(
+      <Link
+        href="https://ko.react.dev"
+        className="button"
+        aria-label="리액트 공식 문서 (번역)"
+      >
+        react.dev
+      </Link>
+    );
+    // screen.debug();
+
+    const link = screen.getByTestId('link');
+    expect(link).toHaveAttribute('aria-label', '리액트 공식 문서 (번역)');
   });
 });
