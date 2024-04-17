d66ab9bfec38bef39f6813da60c156b973c67a7e
yamoo9 <yamoo9@naver.com>
2024-04-17 14:24:52 +0900

    [검증] 사용자 인터페이스에서 UserList 컴포넌트 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { Component } from 'react';
 import { Description, Headline, Link } from './components';
 import { getPublic } from './utils';
+import { UserList } from './components/users/user-list';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      headline: '리액트 러닝 가이드',
-      description:
-        '리액트 라이브러리를 사용해 사용자 인터페이스를 구축하는 방법을 학습합니다.',
-      link: {
-        className: 'button',
-        href: 'https://react.dev',
-        target: '_blank',
-        rel: 'noopener noreferrer',
-        text: 'react.dev',
+  state = {
+    users: [
+      {
+        id: crypto.randomUUID(),
+        name: '리액트',
       },
-    };
-  }
+      {
+        id: crypto.randomUUID(),
+        name: '리액트 라우터',
+      },
+    ],
+    headline: '리액트 러닝 가이드',
+    description:
+      '리액트 라이브러리를 사용해 사용자 인터페이스를 구축하는 방법을 학습합니다.',
+    link: {
+      className: 'button',
+      href: 'https://react.dev',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      text: 'react.dev',
+    },
+  };
 
   render() {
     const { headline, description, link } = this.state;
@@ -49,6 +56,10 @@ class App extends Component {
           />{' '}
           {link.text}
         </Link>
+
+        <UserList users={[]} />
+        <hr />
+        <UserList users={this.state.users} />
       </main>
     );
   }
