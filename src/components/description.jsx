f2755b1f59aae51c88ba0f83cd0c2a906fecd917
yamoo9 <yamoo9@naver.com>
2024-04-17 11:42:44 +0900

    [정정] EMOJI

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -9,30 +9,30 @@
 
 // switch문 조건부 렌더링
 // export function Description({ type, message, children, ...restProps }) {
-//   let imoji = null;
+//   let emoji = null;
 
 //   switch (type) {
 //     default:
 //     case 'greet':
-//       imoji = IMOJI_TYPE.greet;
+//       emoji = EMOJI_TYPE.greet;
 //       break;
 //     case 'angry':
-//       imoji = IMOJI_TYPE.angry;
+//       emoji = EMOJI_TYPE.angry;
 //       break;
 //     case 'embarrassed':
-//       imoji = IMOJI_TYPE.embarrassed;
+//       emoji = EMOJI_TYPE.embarrassed;
 //   }
 
 //   if (message) {
 //     return (
 //       <p {...restProps}>
-//         {imoji} {message}
+//         {emoji} {message}
 //       </p>
 //     );
 //   } else {
 //     return (
 //       <p {...restProps}>
-//         {imoji} {children}
+//         {emoji} {children}
 //       </p>
 //     );
 //   }
@@ -40,41 +40,41 @@
 
 // && 또는 || 연산자 활용 조건부 렌더링
 // export function Description({ type, message, children, ...restProps }) {
-//   const imoji = IMOJI_TYPE[type]; // IMOJI_TYPE['angry']
+//   const emoji = EMOJI_TYPE[type]; // EMOJI_TYPE['angry']
 //   return (
 //     <p {...restProps}>
-//       {imoji} {message || children}
+//       {emoji} {message || children}
 //     </p>
 //   );
 // }
 
 // 터너리(ternary, 삼항 연산자 식 조건 ? 참인 값 : 거짓인 값) 활용
 // export function Description({ type, message, children, ...restProps }) {
-//   const imoji = IMOJI_TYPE[type];
+//   const emoji = EMOJI_TYPE[type];
 //   return (
 //     <p {...restProps}>
-//       {imoji} {message ? message : children}
+//       {emoji} {message ? message : children}
 //     </p>
 //   );
 // }
 
 // null 병합 연산자(??) 활용
 // export function Description({ type, message, children, ...restProps }) {
-//   const imoji = IMOJI_TYPE[type] ?? '🤔';
+//   const emoji = EMOJI_TYPE[type] ?? '🤔';
 //   return (
 //     <p {...restProps}>
-//       {imoji} {message ? message : children}
+//       {emoji} {message ? message : children}
 //     </p>
 //   );
 // }
 
-const printImoji = {
+const printemoji = {
   pin: () => '📌',
   gear: () => '⚙️',
   broccoli: () => '🥦',
 };
 
-const IMOJI_TYPE = {
+const EMOJI_TYPE = {
   greet: { icon: '😃', emotion: '신남' },
   angry: { icon: '😡', emotion: '화남' },
   embarrassed: { icon: '😖', emotion: '난감' },
@@ -82,17 +82,17 @@ const IMOJI_TYPE = {
 
 // 옵셔널 체이닝(?.) 활용
 export function Description({ type, message, children, hide, ...restProps }) {
-  const imoji = IMOJI_TYPE[type] ?? { icon: '🤔', emotion: '생각 중...' };
-  console.log(imoji);
+  const emoji = EMOJI_TYPE[type] ?? { icon: '🤔', emotion: '생각 중...' };
+  console.log(emoji);
 
   let printMessage = `${message ? message : children}`.trim();
 
-  // if (printImoji && typeof printImoji.broccoli === 'function') {
-  printMessage += printImoji.apple?.() ?? '';
+  // if (printemoji && typeof printemoji.broccoli === 'function') {
+  printMessage += printemoji.apple?.() ?? '';
   // }
 
   const isHidden = { display: !hide ? 'block' : 'none' };
-  const { icon, emotion } = imoji;
+  const { icon, emotion } = emoji;
 
   return (
     <p style={isHidden} {...restProps}>
