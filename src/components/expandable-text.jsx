8b76615da9e4ab13fda1fa549c5064f706892b0c
yamoo9 <yamoo9@naver.com>
2024-04-18 10:30:53 +0900

    [실습] ExpandableText 컴포넌트 구현 & 컴포넌트 테스트 슈트 작성

--- src/components/expandable-text.jsx
+++ src/components/expandable-text.jsx
@@ -1,3 +1,19 @@
-export function ExpandableText({ limit = 60, children }) {
-  return <p>{children}</p>;
+import { useState } from 'react';
+
+export const LIMIT = 60;
+
+export function ExpandableText({ limit = LIMIT, children }) {
+  const [isExpand, setIsExpand] = useState(false);
+  const handleExpand = () => setIsExpand(!isExpand);
+
+  if (children.length <= limit) return <p>{children}</p>;
+
+  return (
+    <>
+      {isExpand ? <p>{children}</p> : <p>{children.slice(0, limit)}...</p>}
+      <button type="button" onClick={handleExpand}>
+        {isExpand ? '축소' : '확장'}
+      </button>
+    </>
+  );
 }
--- tests/components/expandable-text.test.jsx
+++ tests/components/expandable-text.test.jsx
@@ -1,19 +1,55 @@
 import { it, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { ExpandableText } from '@/components';
+import userEvent from '@testing-library/user-event';
+import { ExpandableText, LIMIT } from '@/components';
 
 describe('ExpandableText', () => {
-  it.only('축소된 텍스트 렌더링', () => {
-    let textContent = '상태가 변경된 이후 화면이 업데이트 됨을 테스트합니다.';
-    render(<ExpandableText>{textContent}</ExpandableText>);
+  const renderComponent = (text, limit = LIMIT) => {
+    render(<ExpandableText limit={limit}>{text}</ExpandableText>);
+    return {
+      user: userEvent.setup(),
+      paragraph: screen.getByRole('paragraph'),
+      button: screen.queryByRole('button'),
+    };
+  };
 
-    screen.debug();
-    expect(1 + 1).toBe(2);
+  // 렌더링 테스트
+  it('텍스트가 제한 글자수보다 작은 경우, 버튼 요소는 표시되지 않음', () => {
+    let textContent = '화면이 업데이트 됨을 테스트합니다.';
+    const { paragraph } = renderComponent(textContent);
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph.textContent).toHaveLength(textContent.length);
   });
 
-  it('텍스트가 제한 글자수보다 작은 경우 버튼 요소는 표시되지 않음', () => {});
+  it('텍스트가 제한 글자수보다 큰 경우, 버튼 요소는 화면에 표시됨', () => {
+    let text =
+      'ExpandableText 컴포넌트에 상태를 선언한 후, 상호작용에 의해 상태가 변경된 이후 화면이 업데이트 됨을 테스트합니다.';
+    const { button } = renderComponent(text, 40);
+    expect(button).toBeInTheDocument();
+    screen.debug();
+  });
 
-  it('텍스트가 제한 글자수보다 큰 경우 버튼 요소는 화면에 표시됨', () => {});
+  // 상호작용 테스트
+  it('(사용자가) 확장 버튼을 누르면 텍스트 확장, 축소 버튼을 누르면 텍스트 축소됨', async () => {
+    let limit = 36;
+    let text =
+      'ExpandableText 컴포넌트에 상태를 선언한 후, 상호작용에 의해 상태가 변경된 이후 화면이 업데이트 됨을 테스트합니다.';
+    const { user, button, paragraph } = renderComponent(text, limit);
 
-  it('확장 버튼을 누르면 텍스트 확장, 축소 버튼을 누르면 텍스트 축소됨', () => {});
+    // 확장 전, 상황
+    expect(paragraph).toHaveTextContent(text.slice(0, limit) + '...');
+    expect(paragraph.textContent.length).toBeLessThan(text.length);
+
+    // 확장: 사용자가 확장 버튼 클릭
+    await user.click(button);
+    expect(paragraph).toHaveTextContent(text);
+    expect(paragraph.textContent).toHaveLength(text.length);
+    expect(button).toHaveTextContent(/축소/);
+
+    // 축소: 사용자가 확장 버튼 클릭
+    await user.click(button);
+    expect(paragraph).toHaveTextContent(text.slice(0, limit) + '...');
+    expect(paragraph.textContent.length).toBeLessThan(text.length);
+    expect(button).toHaveTextContent(/확장/);
+  });
 });
