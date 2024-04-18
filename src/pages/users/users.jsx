c0ba2f463f99da6044496e08ba853467ba36387e
yamoo9 <yamoo9@naver.com>
2024-04-18 17:52:53 +0900

    [학습] state vs. ref vs. variable

--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -1,11 +1,12 @@
 import { UserList, UserListCount, UserSearchBox } from '@/components';
-import { useEffect, useState } from 'react';
+import { Component, useEffect, useRef, useState } from 'react';
 import classes from './users.module.css';
 
 export function UsersPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
     setIsLoading(true);
     setError(null);
@@ -36,10 +37,8 @@ export function UsersPage() {
   }, []);
 
   const [searchedUsers, setSearchedUsers] = useState(users);
-  useEffect(() => {
-    setSearchedUsers(users);
-  }, [users]);
 
+  // 이벤트
   const handleChange = (search) => {
     setSearchedUsers(
       search !== 'reset'
@@ -48,13 +47,33 @@ export function UsersPage() {
     );
   };
 
-  // if (isLoading) {
-  //   return <div role="alert">데이터 로딩 중...</div>;
-  // }
+  // 이펙트
+  useEffect(() => {
+    if (users) {
+      setSearchedUsers(users);
+    }
+  }, [users]);
 
-  // if (error) {
-  //   return <div role="alert">{error.message}</div>;
-  // }
+  // 상태(변경되면 리-렌더 ✅ 및 값 기억)
+  const [count, setCount] = useState(9);
+  const handleCountUp = () => {
+    setCount((c) => c + 1);
+    setCount((c) => c + 1);
+    setCount((c) => c + 1);
+  };
+
+  // 참조(변경되도 리-렌더 ❌ 및 값 기억)
+  const countRef = useRef(9);
+  const handleCountRefUp = () => {
+    countRef.current += 3;
+    console.log('countRef.current = ', countRef.current);
+  };
+
+  let countVariable = 9;
+  const handleCountVariableUp = () => {
+    countVariable += 3;
+    console.log('countVariable = ', countVariable);
+  };
 
   const userList = isLoading ? (
     <div role="alert" className={classes.loading}>
@@ -70,9 +89,56 @@ export function UsersPage() {
 
   return (
     <div className={classes.component}>
+      <button type="button" onClick={handleCountUp}>
+        count = {count} (+3)
+      </button>
+      <button type="button" onClick={handleCountRefUp}>
+        countRef.current = {countRef.current} (+3)
+      </button>
+      <button type="button" onClick={handleCountVariableUp}>
+        countVariable = {countVariable} (+3)
+      </button>
+
       <UserSearchBox onChange={handleChange} />
       {userList}
       <UserListCount count={searchedUsers.length} total={users.length} />
     </div>
   );
+}
+
+export class UsersPAGE extends Component {
+  state = {
+    count: 9,
+  };
+
+  count = 9; // useRef()
+
+  render() {
+    return (
+      <>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState(
+              (state) => ({ count: state.count + 1 }),
+              // effect
+              () => {
+                console.log(this.state.count); // 10
+              }
+            );
+          }}
+        >
+          change state
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.count += 1;
+          }}
+        >
+          change instance member
+        </button>
+      </>
+    );
+  }
 }
