1f0063a28bb29b1380a28e1ebc5057ea28e4c365
yamoo9 <yamoo9@naver.com>
2024-04-18 14:17:00 +0900

    [구성] Tailwind CSS 프레임워크

--- src/pages/users/components/user-search-box.jsx
+++ src/pages/users/components/user-search-box.jsx
@@ -1,5 +1,5 @@
 import { useId, useState } from 'react';
-import S from './user-search-box.module.css';
+// import S from './user-search-box.module.css';
 
 export function UserSearchBox({ onChange }) {
   const id = useId();
@@ -20,9 +20,15 @@ export function UserSearchBox({ onChange }) {
   };
 
   return (
-    <div className={S.component}>
-      <label htmlFor={id}>사용자 검색</label>
+    <div
+      className="border border-solid border-sky-500 rounded-full text-sm"
+      // className={S.component}
+    >
+      <label htmlFor={id} className="sr-only">
+        사용자 검색
+      </label>
       <input
+        className="w-full px-3 py-2 rounded-full"
         type="search"
         placeholder="사용자 이름 입력"
         id={id}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// JIT 모드 활성화
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.jsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
