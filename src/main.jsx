47a2601181b65a4d97b53659559493482776423f
yamoo9 <yamoo9@naver.com>
2024-04-18 11:46:28 +0900

    [구성] StrictMode
    - 컴포넌트 순수성 검사
    - 예측 불가능한 버그 방지
    - 오래된 APIs 경고

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import './styles/globals.css';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
-  createRoot(rootElement).render(<App />);
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 } else {
   alert('id 속성 이름이 "root"인 요소가 문서에 존재하지 않습니다.');
 }
