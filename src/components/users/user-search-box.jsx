2967dbae5ca9be2342b381305e1b9ee3989655ab
yamoo9 <yamoo9@naver.com>
2024-04-19 10:32:02 +0900

    [학습] useLayoutEffect vs. useEffect

--- src/components/users/user-search-box.jsx
+++ src/components/users/user-search-box.jsx
@@ -1,5 +1,5 @@
 import classes from './user-search-box.module.css';
-import { useId, useState } from 'react';
+import { useEffect, useId, useLayoutEffect, useRef, useState } from 'react';
 
 export function UserSearchBox({ onChange }) {
   const id = useId();
@@ -19,12 +19,41 @@ export function UserSearchBox({ onChange }) {
     }
   };
 
+  // useLayoutEffect vs. useEffect
+  //
+  // render trigger (set state function)
+  // component re-render (react)
+  // 1. render
+  // 1.1 useLayoutEffect Cleanup
+  // 1.2 useEffect Cleanup
+  // 2. dom commit
+  // 2.1 useLayoutEffect
+  // 3. browser painting
+  // 3.1 useEffect
+
+  const searchInputRef = useRef(null);
+
+  useEffect(() => {
+    console.log('effect: ', searchInputRef.current);
+    return () => {
+      console.log('effect cleanup');
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log('layout effect: ', searchInputRef.current);
+    return () => {
+      console.log('layout effect cleanup');
+    };
+  }, []);
+
   return (
     <div className={classes.component}>
       <label htmlFor={id} className="sr-only">
         사용자 검색
       </label>
       <input
+        ref={searchInputRef}
         type="search"
         placeholder="사용자 이름 입력"
         id={id}
